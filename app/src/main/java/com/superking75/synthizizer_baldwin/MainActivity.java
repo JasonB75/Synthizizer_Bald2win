ec3f603ac79d2d8f8ac47429d82c964bf2eb905c
JasonB75 <37903292+JasonB75@users.noreply.github.com>
2019-03-27 17:12:41 -0400

    Adding comments and the switch.

--- app/src/main/java/com/superking75/synthizizer_baldwin/MainActivity.java
+++ app/src/main/java/com/superking75/synthizizer_baldwin/MainActivity.java
@@ -2,26 +2,22 @@ package com.superking75.synthizizer_baldwin;
 
 //https://abhiandroid.com/ui/switch
 
-import android.media.MediaPlayer;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.NumberPicker;
 import android.widget.Switch;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getName();
 
 
     /**
-     * Creates the length of all the notes that will be used later in the program.
+     * Creation of the length of all the notes that will be used later in the program.
      */
     private final int WHOLE_NOTE = 4000;
     private final int HALF_NOTE = WHOLE_NOTE/2;
@@ -30,100 +26,107 @@ public class MainActivity extends AppCompatActivity {
     private final int SIXTEEN_NOTE  = WHOLE_NOTE/16;
 
     /**
-     * Creates the instances of all the variables and buttons that will be put into effect later on in the program
+     * Creation of the instances of the buttons, numberpickers, and switches that will be put into effect later on in the program
      */
-    private Button button1;
-    private Button button2;
+    private Button playEbutton;
+    private Button eScaleButton;
     private Button noteNumberButton;
     private Button button3;
     private NumberPicker noteNumber;
     private NumberPicker notePicker;
     private NumberPicker twinkletimes;
     private  Button button5;
-
     private Switch switch1;
 
-    private MediaPlayer mpA;
-    private MediaPlayer mpB;
-    private MediaPlayer mpBb;
-    private MediaPlayer mpC;
-    private MediaPlayer mpCs;
-    private MediaPlayer mpD;
-    private MediaPlayer mpDs;
-    private MediaPlayer mpE;
-    private MediaPlayer mpF;
-    private MediaPlayer mpFs;
-    private MediaPlayer mpG;
-    private MediaPlayer mpGs;
-    private MediaPlayer mpHighE;
-    private MediaPlayer mpHighF;
-    private MediaPlayer mpHighFs;
-    private MediaPlayer mpHighG;
-    private MediaPlayerThread mpt;
-    int noteNoteNumber = 1;
-    int notepicked = 0;
-    int twinkleLineTwo = 1;
+    private MediaPlayerThread mpt; // Creation of the variable used for the .playnote() method.
+
+    /**
+     * Creation of most-recent-variables used later in the program
+     */
+    int timesNotePlayed = 1; //How many times will the note be played
+    int notepicked = 0; // Number variable of the selected note
+    int twinkleLineTwo = 1; // How many times the second line will be played
+    boolean playingLine2 = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        button1 = (Button)findViewById(R.id.button1);
-        button2 = (Button)findViewById(R.id.button2);
+
+
+        /**
+         * Referencing each xml view object to it's java variable.
+         */
+        playEbutton = (Button)findViewById(R.id.button1);
+        eScaleButton = (Button)findViewById(R.id.button2);
         noteNumberButton = (Button)findViewById(R.id.noteNumberButton);
         button3= (Button)findViewById(R.id.button3);
         button5= (Button) findViewById(R.id.button5);
         final TextView main = (TextView) findViewById(R.id.text1);
-        noteNumber = (NumberPicker) findViewById(R.id.np);
-        notePicker = (NumberPicker) findViewById(R.id.note);
-        twinkletimes = (NumberPicker)findViewById(R.id.twinkle);
+        noteNumber = (NumberPicker) findViewById(R.id.noteNumber);
+        notePicker = (NumberPicker) findViewById(R.id.notePicker);
+        twinkletimes = (NumberPicker)findViewById(R.id.twinkleTimePicker);
+        switch1 = (Switch)findViewById(R.id.switch1);
+
+
+        /**
+         * Sets the features of the 3 number picker objects
+         */
         twinkletimes.setMinValue(1);
         twinkletimes.setMaxValue(5);
         twinkletimes.setWrapSelectorWheel(true);
-        final String[] noteValues = {"a", "b", "Bb", "C", "C#", "d", "d#", "e", "f", "f#", "g", "g#", "High E", "High F#", "High G"};
+
         notePicker.setMinValue(0);
+        final String[] noteValues = {"a", "b", "Bb", "C", "C#", "d", "d#", "e", "f", "f#", "g", "g#", "High E", "High F#", "High G"};
         notePicker.setMaxValue(noteValues.length-1);
         notePicker.setDisplayedValues(noteValues);
         notePicker.setWrapSelectorWheel(true);
 
-        final MediaPlayer[] noteList ={mpA, mpB, mpBb, mpC, mpCs, mpD, mpDs, mpE, mpF, mpFs,mpG, mpGs, mpHighE, mpHighFs, mpHighG};
+        noteNumber.setMinValue(1);
+        noteNumber.setMaxValue(10);
+        noteNumber.setWrapSelectorWheel(true);
+
+        /**
+         * Lists of note id's to be used in the playNote() method.
+         */
         final int[] allNoteId = {R.raw.scalea,R.raw.scaleb,R.raw.scalebb,R.raw.scalec,R.raw.scalecs,R.raw.scaled,R.raw.scaleds,R.raw.scalee,R.raw.scalef,R.raw.scalefs,R.raw.scaleg,R.raw.scalegs,R.raw.scalehighe,R.raw.scalehighfs,R.raw.scalehighg};
         final int[] noteIdListE = {R.raw.scalee,R.raw.scalefs,R.raw.scalegs,R.raw.scalea,R.raw.scaleb,R.raw.scalecs,R.raw.scaleds,R.raw.scalehighe};
-        //High E, High E, D, D, C Sharp, C Sharp, B
-        final int[] twinkleList1 = {R.raw.scalea, R.raw.scalea, R.raw.scalehighe,R.raw.scalehighe,R.raw.scalehighfs,R.raw.scalehighfs,R.raw.scalehighe,R.raw.scaled,R.raw.scaled,R.raw.scalecs,R.raw.scalecs,R.raw.scaleb,R.raw.scaleb,R.raw.scalea};
-        final int[] twinkleList2 = {R.raw.scalehighe,R.raw.scalehighe,R.raw.scaled,R.raw.scaled,R.raw.scalecs,R.raw.scalecs,R.raw.scaleb};
-        final int[] twinkleTimes2 = {750,750,750,750,750,750,1500};
-        final int[] twinkleTimes = {750,750,750,750,750,750,1500,750,750,750,750 ,750,750,1500};
-
-        final int[] bitesDust = {R.raw.scalee,R.raw.scalee,R.raw.scalee,R.raw.scalee,R.raw.scalee,R.raw.scalee,R.raw.scaleg,R.raw.scalee,R.raw.scalea,R.raw.scalee,R.raw.scalee,R.raw.scalee,R.raw.scalee,R.raw.scalee,R.raw.scalee,R.raw.scaleg,R.raw.scalee,R.raw.scalea,
+        final int[] twinkleNotesLine1 = {R.raw.scalea, R.raw.scalea, R.raw.scalehighe,R.raw.scalehighe,R.raw.scalehighfs,R.raw.scalehighfs,R.raw.scalehighe,R.raw.scaled,R.raw.scaled,R.raw.scalecs,R.raw.scalecs,R.raw.scaleb,R.raw.scaleb,R.raw.scalea};
+        final int[] twinkleNotesLine2 = {R.raw.scalehighe,R.raw.scalehighe,R.raw.scaled,R.raw.scaled,R.raw.scalecs,R.raw.scalecs,R.raw.scaleb};
+        final int[] bitesDustNotes = {R.raw.scalee,R.raw.scalee,R.raw.scalee,R.raw.scalee,R.raw.scalee,R.raw.scalee,R.raw.scaleg,R.raw.scalee,R.raw.scalea,R.raw.scalee,R.raw.scalee,R.raw.scalee,R.raw.scalee,R.raw.scalee,R.raw.scalee,R.raw.scaleg,R.raw.scalee,R.raw.scalea,
                 R.raw.scalee,R.raw.scalee,R.raw.scalee,R.raw.scalee,R.raw.scalee,R.raw.scaleg,R.raw.scalee,R.raw.scalee,R.raw.scalee,R.raw.scalee,R.raw.scaleb,R.raw.scaleb,R.raw.scalea,R.raw.scaleb,R.raw.scalea};
-        final int[] bitesNums = {QUARTER_NOTE,QUARTER_NOTE,QUARTER_NOTE,SIXTEEN_NOTE,EIGHT_NOTE,EIGHT_NOTE,EIGHT_NOTE,EIGHT_NOTE,SIXTEEN_NOTE+QUARTER_NOTE,
+
+        /**
+         * Lists of times / note delays that line up with the specified note id's in the above arrays
+         */
+        final int[] twinkleTimesLine2 = {750,750,750,750,750,750,1500};
+        final int[] twinkleTimesLine1 = {750,750,750,750,750,750,1500,750,750,750,750 ,750,750,1500};
+       final int[] bitesDustTimes = {QUARTER_NOTE,QUARTER_NOTE,QUARTER_NOTE,SIXTEEN_NOTE,EIGHT_NOTE,EIGHT_NOTE,EIGHT_NOTE,EIGHT_NOTE,SIXTEEN_NOTE+QUARTER_NOTE,
                 QUARTER_NOTE,QUARTER_NOTE,QUARTER_NOTE,SIXTEEN_NOTE,EIGHT_NOTE,EIGHT_NOTE,EIGHT_NOTE,EIGHT_NOTE,SIXTEEN_NOTE+QUARTER_NOTE,
                 EIGHT_NOTE,EIGHT_NOTE,SIXTEEN_NOTE,SIXTEEN_NOTE,SIXTEEN_NOTE,SIXTEEN_NOTE+EIGHT_NOTE,EIGHT_NOTE,EIGHT_NOTE,SIXTEEN_NOTE,SIXTEEN_NOTE,
                 EIGHT_NOTE,EIGHT_NOTE,SIXTEEN_NOTE,EIGHT_NOTE,SIXTEEN_NOTE+QUARTER_NOTE,
 
         };
 
-
-        mpt = new MediaPlayerThread(MainActivity.this);
-
-        //fi
-        // nal MediaPlayer[] twinkleArray = {mpA, mpB};
+        mpt = new MediaPlayerThread(MainActivity.this); // MediaPlayer object used to run the playNote() method
 
 
-        noteNumber.setMinValue(1);
-        noteNumber.setMaxValue(10);
-        noteNumber.setWrapSelectorWheel(true);
-
-
+        /**
+         * When the NumberPicker is changed this sets the appropreate variable to the times the selected note will play.
+         */
         noteNumber.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker picker, int oldVal, int newVal){
                 //Display the newly selected number from picker
-                noteNoteNumber = newVal;
+                timesNotePlayed = newVal;
                 //main.setText(newVal);
             }
         });
+
+        /**
+         * When the numbePicker is changed, the appropriate variable will be set to the number of times the 2nd line of Twinkle will be played.
+         */
         twinkletimes.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker picker, int oldVal, int newVal){
@@ -133,6 +136,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        /**
+         * When the numbePicker is changed, the appropriate variable will be set to the index of the specified note to be played.
+         */
         notePicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker picker, int oldVal, int newVal){
@@ -141,17 +147,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-        button1.setOnClickListener(new View.OnClickListener() {
+        /**
+         * Plays the single E when pressed
+         */
+        playEbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mpE.seekTo(0);
+                mpt.playNote(R.raw.scalee, 1000);
                 Log.e(  TAG, "Button 1 clicked");
-                mpE.start();
+
             }
         });
 
-        button2.setOnClickListener(new View.OnClickListener() {
+        /**
+         * Plays the E scale when pressed
+         */
+        eScaleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //int stepper = 0;
@@ -167,53 +178,58 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        /**
+         * References the value from 'notePicked' to the index of all avaliable notes while running through a loop. Said loop runs for the number of times specifed by 'timesNotePlayed'
+         */
         noteNumberButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int stepper = 0;
 
 
-                while (stepper < noteNoteNumber)
+                while (stepper < timesNotePlayed)
                 {
                     mpt.playNote(allNoteId[notepicked], 1000);
-                   // noteList[notepicked].seekTo(0);
-                    //noteList[notepicked].start();
-                   // delayPlaying(WHOLE_NOTE);
-                  //  temp[notepicked].stop();
                     stepper++;
-
                 }
 
             }
         });
+
+
+
+
+        /**
+         * Plays Twinkle Twinkle Little Star. It does this with for each loops for each line of the songs, here it uses the note with it's delay to call the playNote method.
+         *
+         */
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
 
             public void onClick(View v) {
                 int stepper = 0;
-                for (int n: twinkleList1)
+                for (int n: twinkleNotesLine1) //Line 1
                 {
 
-                    mpt.playNote(n, twinkleTimes[stepper]);
+                    mpt.playNote(n, twinkleTimesLine1[stepper]);
                     stepper++;
 
                 }
-
+                if (switch1.isChecked())
+                {
                 stepper=0;
-                //for(int i = 0; i< twinkleLineTwo;i++)
-                //{
-                    for (int n : twinkleList2) {
-
-                        mpt.playNote(n, twinkleTimes2[stepper]);
+                    for (int n : twinkleNotesLine2) //line 2
+                    {
+                        mpt.playNote(n, twinkleTimesLine2[stepper]);
                         stepper++;
 
                     }
-             //   }
+               }
                stepper = 0;
-                for (int n: twinkleList1)
+                for (int n: twinkleNotesLine1) // line 3
                 {
 
-                    mpt.playNote(n, twinkleTimes[stepper]);
+                    mpt.playNote(n, twinkleTimesLine1[stepper]);
                     stepper++;
 
                 }
@@ -221,14 +237,19 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-button5.setOnClickListener(new View.OnClickListener() {
+
+        /**
+         * When clicked this plays a section of another one bites the dust by running through each element of the
+         * array containing the note id's and the cosoponding note times. It put's these into the playNote method to play them
+         */
+        button5.setOnClickListener(new View.OnClickListener() {
     @Override
     public void onClick(View v) {
         int stepper = 0;
-        for (int n: bitesDust)
+        for (int n: bitesDustNotes)
         {
 
-            mpt.playNote(n, bitesNums[stepper]);
+            mpt.playNote(n, bitesDustTimes[stepper]);
             stepper++;
 
         }
